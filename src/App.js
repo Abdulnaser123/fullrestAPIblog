2c00a940861feb171f567e2256d1c763909d8336
Marah-Odeh <marah.majed.s@gmail.com>
2022-11-26 11:55:40 +0200

    solve conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import HomePage from "./views/HomePage";
-import Mostcommonposts from "./views/MostCommonPosts";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
-      {/* <Mostcommonposts /> */}
     </div>
   );
 }
